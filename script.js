757350e83d45193aa886a50131eb4ce6bb3c2a49
B-ELHAFID <132234730+B-ELHAFID@users.noreply.github.com>
2024-10-30 22:22:21 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-function openLightbox(imageSrc) {
-    document.getElementById('lightbox').style.display = 'flex';
-    document.getElementById('lightboxImage').src = imageSrc;
-}
-
-function closeLightbox() {
-    document.getElementById('lightbox').style.display = 'none';
-}
